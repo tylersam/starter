a9d0d8f2a1629e453a1c535339f8cca38a20a8fd
Tyler <tylersam@gmail.com>
2015-02-24 12:35:10 -0600

    Clean up oauth process

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,9 +2,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
 
-    # You need to implement the method (find_for_google_oauth2) below in your model (e.g. app/models/user.rb)
-    data = request.env['omniauth.auth'].info
-    if User.where(:email => data['email']).exists?
+    # method (find_for_google_oauth2) is in User model
+
+    #The user must have already been created in the database (i.e. admin can create users with an email -
+    # when the oauth process happens, this user is merged with the google oauth user)
+    if User.where(:email => request.env['omniauth.auth'].info['email']).exists?
       @user = User.find_for_google_oauth2(request.env['omniauth.auth'], current_user)
 
       if @user.persisted?
@@ -12,11 +14,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         sign_in_and_redirect @user, :event => :authentication
       else
         session["devise.google_data"] = request.env["omniauth.auth"]
+
         redirect_to new_user_registration_url
       end
 
     else
-      flash[:error] = "Not on the authorized user list. Please contact Tyler if this is an error."
+      flash[:error] = 'Not on the authorized user list. Please contact Tyler if this is an error.'
       redirect_to root_path
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,6 @@ class User < ActiveRecord::Base
 
   after_initialize :set_default_role, :if => :new_record?
 
-  def set_default_role
-    self.role ||= :manager
-  end
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -14,26 +10,35 @@ class User < ActiveRecord::Base
          :omniauthable, :omniauth_providers => [:google_oauth2]
 
 
+  def set_default_role
+    self.role ||= :manager
+  end
+
   # method called in the users\omniauth_callback_controller.rb file
   # in that controller, the user's eamil is checked against the list of
   # approved_users. An error message saying  Not on the authorized list
   # is displayed if they are approved.
-  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
-    data = access_token.info
-    token = access_token.credentials["token"]
-    refresh_token = access_token.credentials["refresh_token"]
+  def self.find_for_google_oauth2(request_response, signed_in_resource=nil)
+    data = request_response.info
+    token = request_response.credentials['token']
+    refresh_token = request_response.credentials['refresh_token']
 
-    user = User.where(:email => data["email"]).first
+    user = User.where(:email => data['email']).first
 
-    #The user must have already been created in the system
-    if user
+
       user.name = data['name']
       user.first_name = data['first_name']
       user.last_name = data['last_name']
       user.token = token
-      user.refresh_token = refresh_token
+      user.refresh_token = refresh_token unless refresh_token.nil?
       user.save!
-    end
+
     user
   end
+
+
+  def refresh_token_present?
+    !refresh_token.nil?
+  end
+
 end
